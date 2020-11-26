906eceb9a1a042ce4e67578cd1e361edd01b6936
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-11-26 09:41:10 +0200

    Add files via upload

---
+++ 13_asa01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+   auth = HTTPBasicAuth('ntc', 'ntc123')
+   headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+   }
+
+   requests.packages.urllib3.disable_warnings()
+
+   payload = {
+      "kind": "object#GigabitInterface",
+      "interfaceDesc": "Configured by Python"
+   }
+
+url = "https://asav/api/interfaces/physical/GigabitEthernet0_API_SLASH_0"
+response = requests.patch(url, data=json.dumps(payload), auth=auth, headers=headers, verify=False)
