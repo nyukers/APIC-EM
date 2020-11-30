23bf621692c1df69de11163edaaa5de405e6227a
Nyukers <26322892+nyukers@users.noreply.github.com>
2020-11-30 14:07:55 +0200

    Create 18_eos-napalm.py

---
+++ NAPALM/18_eos-napalm.py
@@ -0,0 +1,14 @@
+#
+# NAPALM Arista EOS preview
+#
+
+from napalm import get_network_driver
+
+driver = get_network_driver('eos')
+device = driver('eos-spine1', 'ntc', 'ntc123')
+
+# device – переменная, содержащая объект устройства NAPALM. Этот объект имеет методы для работы с конфигурациями устройства, в том числе
+# и для выполнения операции замены конфигурации. Операция выполняется с помощью метода load_replace_candidate().
+
+device.open() # требуется для загрузки регистрационных данных и установления соединения с устройством на основе используемого API
+device.load_replace_candidate(filename='eos-spine1.conf')
